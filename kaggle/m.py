629ae33c045032f692327610eefe143a5830dcae
Clemens Näther <naether.clemens@gmail.com>
2024-11-25 21:42:55 +0100

    added world_cup_wins.py

--- kaggle/m.py
+++ kaggle/m.py
@@ -3,13 +3,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Import data
 #import kagglehub
 #Letzte Version des Datensatzes herunterladen
 #dataset = "piterfm/fifa-football-world-cup"
 #path = kagglehub.dataset_download(dataset)
 #print("Path to dataset files:", path)
+# Import data
 
-path = r"C:\Users\clemi\.cache\kagglehub\datasets\piterfm\fifa-football-world-cup\versions\24"
+#path = r"C:\Users\clemi\.cache\kagglehub\datasets\piterfm\fifa-football-world-cup\versions\24"
 
 # Load data
 matches = pd.read_csv(path + "/matches_1930_2022.csv")
