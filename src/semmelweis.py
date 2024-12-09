98fbf351c8fa2a8e5c8197f08dddb9f0fb062ba2
Clemens Näther <naether.clemens@gmail.com>
2024-12-09 14:14:49 +0100

    diff done bayes example

--- src/semmelweis.py
+++ src/semmelweis.py
@@ -2,14 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import beta
 
+m = 0.03
+v = 0.021
+f_v_m = 0.5
+
+ln_f_v_m = np.log(f_v_m)
+ln_ratio_v_m = np.log(v / m)
+ln_ratio_1_v_1_m = np.log((1 - v) / (1 - m))
+factor = (1 - m) / m
+
 x = 1989
 n_x = 20042
 y = 1883
 n_y = 56104
 
 # Priorparameter
-a_prior = 10
-b_prior = 438
+a_prior = ln_f_v_m / (ln_ratio_v_m + factor * ln_ratio_1_v_1_m) + 1
+b_prior = (a_prior-1) * factor + 1
 
 # Posteriorschätzer für Pi_X
 a_X = a_prior + x
@@ -26,6 +35,17 @@ prior = beta.pdf(xl, a_prior, b_prior)
 posterior_X = beta.pdf(xl, a_X, b_X)
 posterior_Y = beta.pdf(xl, a_Y, b_Y)
 
+#Calculate the 99% credible intervals
+ci_X = beta.interval(0.99, a_X, b_X)
+ci_Y = beta.interval(0.99, a_Y, b_Y)
+
+ci_X = (round(ci_X[0], 4), round(ci_X[1], 4))
+ci_Y = (round(ci_Y[0], 4), round(ci_Y[1], 4))
+
+print('99% Kredibilitätsintervall für X:', ci_X)
+print('99% Kredibilitätsintervall für Y:', ci_Y)
+
+# Plot the prior and posterior distributions
 plt.figure(figsize=(10, 6))
 plt.plot(xl, prior, label='Prioriverteilung', color='blue', linestyle='--', linewidth=2)
 plt.plot(xl, posterior_X, label='Posterioriverteilung für X', color='green', linewidth=2)
